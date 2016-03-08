460452ad7b5e2ea76858e5c0795c35658ef80e3a
Drew Fleeman <drew.fleeman@gmail.com>
2016-03-07 16:02:05 -0800

    Use tile instances for constructing board positions

--- board.rb
+++ board.rb
@@ -1,20 +1,47 @@
+require_relative 'tile.rb'
+
 class Board
   attr_accessor :grid
 
   def initialize(size, num_bombs)
-    @grid = Array.new(size) { Array.new(size, "_") }
-    setup_board(num_bombs)
+    @grid = Array.new(size) { Array.new(size, nil) }
+    setup_board
+    lay_bombs(num_bombs)
   end
 
-  def setup_board(num_bombs)
+  def setup_board
+    @grid.each_index do |row|
+      @grid.each_index do |col|
+        @grid[row][col] = Tile.new(@grid, [row, col])
+      end
+    end
+  end
+
+  def lay_bombs(num_bombs)
     bombs_laid = 0
     while bombs_laid < num_bombs
       random_row = rand(@grid.count)
       random_col = rand(@grid.count)
-      if @grid[random_row][random_col] != "B"
-        @grid[random_row][random_col] = "B"
+      if @grid[random_row][random_col].bombed == false
+        @grid[random_row][random_col].bombed = true
         bombs_laid += 1
       end
     end
   end
+
+  def render_board
+    to_render = ""
+    @grid.each_index do |row|
+      @grid.each_index do |col|
+        to_render += @grid[row][col].draw
+      end
+      to_render += "\n"
+    end
+
+    print to_render
+  end
 end
+
+b = Board.new(9, 2)
+
+b.render_board
--- tile.rb
+++ tile.rb
@@ -1,12 +1,21 @@
 class Tile
   attr_accessor :flagged, :bombed, :revealed
-  
+
   def initialize(board, position)
     @board = board
-    @position = position
+    @row = position[0]
+    @col = position[1]
     @flagged = false
     @bombed = false
     @revealed = false
+  end
+
+  def draw
+    if self.bombed
+      return ' B '
+    else
+      return ' _ '
+    end
   end
 
   def reveal
